11c855ce8da45b026890500b660c11430a76fb8c
e <593601399@qq.com>
2019-04-09 16:38:37 +0800

    All

---
+++ venv/Lib/_testimportmultiple.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# module _testimportmultiple
+# from A:\Study\Environment\python3.7\DLLs\_testimportmultiple.pyd
+# by generator 1.145
+""" _testimportmultiple doc """
+# no imports
+
+# no functions
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is ''
+
+__spec__ = None # (!) real value is ''
+
---
+++ venv/Lib/cython_runtime.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# module cython_runtime
+# from A:\Study\Document\WorkSpace\Github\Aircraft-Wars\venv\lib\site-packages\pygame\pypm.cp37-win_amd64.pyd
+# by generator 1.145
+# no doc
+# no imports
+
+# Variables with simple values
+
+__loader__ = None
+
+__spec__ = None
+
+# no functions
+# no classes
